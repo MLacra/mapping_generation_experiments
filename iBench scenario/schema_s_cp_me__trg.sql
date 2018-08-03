2b7fb296e4e353793fac34a47d7547a15063f5c6
Lara <lacramioaramazilu@gmail.com>
2018-08-03 16:27:05 +0100

    synthetic scenarios - added profile data for join scenarios

---
+++ iBench scenario/schema_s_cp_me__trg.sql
@@ -0,0 +1,9 @@
+DROP SCHEMA IF EXISTS s_cp_me__trg CASCADE;
+CREATE SCHEMA s_cp_me__trg;
+CREATE TABLE s_cp_me__trg.test_le_1_nl0_ce0_target_relation (
+test_le_1_nl0_ce0_t_attribute1 text NOT NULL, 
+test_le_1_nl0_ce0_t_attribute3 text NOT NULL, 
+test_le_1_nl0_ce0_t_attribute2 text NOT NULL, 
+test_le_1_nl0_ce0_t_attribute5 text NOT NULL PRIMARY KEY, 
+test_le_1_nl0_ce0_t_attribute4 text NOT NULL);
+
